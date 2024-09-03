4af664cc74b319c52c161c98ad6e810f411943db
AmeyaSoman1 <89114270+AmeyaSoman1@users.noreply.github.com>
2024-09-03 12:45:02 -0400

    Update keys.py
    To not make private info known on a public repo.

--- keys.py
+++ keys.py
@@ -1,5 +1,5 @@
-account_sid = 'AC8e22f32862a6574530661344a96da8f2'
-auth_token = '6429c169a1588d0b5421b8dc8c59ee5f'
+account_sid = 'XXX' #twilio issued
+auth_token = 'XXX' #twilio issued
 
-twilio_number = '+14154087784'
-target_number = '+16472253438'
+twilio_number = '+1XXXXXXXXX' #twilio issued number
+target_number = '+1XXXXXXXXX' #personal/target number
